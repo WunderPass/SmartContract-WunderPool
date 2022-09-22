0fd79352c868b2be9cc9e11afc05b3cd76f40aa7
Moritz Löchner <momo.loechner@gmail.com>
2022-09-22 15:46:26 +0200

    Token Swapper Test

---
+++ govTokenArgs.js
@@ -0,0 +1,12 @@
+module.exports = [
+  // string name
+  'First Zeta Pool Token',
+  // string symbol
+  'FIR',
+  // uint256 _amount
+  '3000000',
+  // address _creator
+  '0x0D113d629db84c26c56cd225b878BbE3af6C2CF6',
+  // address _launcher
+  '0xB5Ae136D3817d8116Fce70Ac47e856fc484dafAe',
+];
---
+++ poolArgs.js
@@ -0,0 +1,19 @@
+module.exports = [
+  // string _name,
+  'First Zeta Pool',
+  // address _launcher,
+  '0xB5Ae136D3817d8116Fce70Ac47e856fc484dafAe',
+  // address _governanceToken,
+  '0x90e54dae2aeea04f1a403924bd10e20fff6921fa',
+  // address _creator,
+  '0x0D113d629db84c26c56cd225b878BbE3af6C2CF6',
+  // address[] _members,
+  [
+    '0x7E0b49362897706290b7312D0b0902a1629397D8',
+    '0x1A8459f9dDEcABE92281EbdFa62874010A53FdC6',
+    '0x097bF9D9A2C838e12fE153e4d7F83B48AdB572C6',
+    '0x138c65abF83b45AE1F3E7fdCc8C56D8997779580',
+  ],
+  // uint256 _amount
+  '3000000',
+];
