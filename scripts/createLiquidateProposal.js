3d817c48d6b79be0ab52cf28a8bb37e16424162e
Moritz Löchner <momo.loechner@gmail.com>
2022-10-24 11:22:48 +0200

    Script for manual pool liquidation

---
+++ scripts/createLiquidateProposal.js
@@ -0,0 +1,44 @@
+const { createProposal } = require('../test/backend');
+
+const poolAddress = '0x3ad9d892e16a7f66b2e58ea45e223527d3eb565d';
+
+async function main() {
+  if (poolAddress.length < 42) throw 'Invalid Pool Address';
+
+  const [user] = await ethers.getSigners();
+
+  const pool = await ethers.getContractAt(`WunderPoolEta`, poolAddress, user);
+  await pool
+    .name()
+    .then(async (name) => {
+      console.log(`Creating Liquidate Proposal for Pool "${name}"`);
+      await createProposal(
+        user,
+        pool,
+        user,
+        'Liquidate The Pool',
+        'Lets get out',
+        [pool.address],
+        ['liquidatePool()'],
+        ['0x']
+      )
+        .then((tx) => {
+          console.log(`✅ Created Proposal`);
+        })
+        .catch((err) => {
+          console.log(`⚠️  ${err.error.message}`);
+        });
+    })
+    .catch((err) => {
+      console.log('Pool does not exist');
+    });
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
